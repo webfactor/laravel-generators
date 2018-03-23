a2b46b36420844ef418b6d653deaa64b66af748f
Thomas Swonke <thomas.swonke@webfactor.de>
2018-03-23 15:17:11 +0100

    [EH] set $removeNumericIndex default to true

--- src/Helper/ShortSyntaxArray.php
+++ src/Helper/ShortSyntaxArray.php
@@ -4,7 +4,7 @@ namespace Webfactor\Laravel\Generators\Helper;
 
 class ShortSyntaxArray
 {
-    public static function parse(array $expression, $removeNumericIndex = false, $indent = 4): string
+    public static function parse(array $expression, $removeNumericIndex = true, $indent = 4): string
     {
         $object = json_decode(str_replace(['(', ')'], ['&#40', '&#41'], json_encode($expression)), true);
         $export = str_replace(['array (', ')', '&#40', '&#41'], ['[', ']', '(', ')'], var_export($object, true));

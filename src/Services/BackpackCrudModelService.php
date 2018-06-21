667b39d34b39bf70ef7559e38be1df0e63b626b3
Thomas Swonke <thomas.swonke@webfactor.de>
2018-06-21 15:24:25 +0200

    [EH] reimplement crud model - wip

--- src/Services/BackpackCrudModelService.php
+++ src/Services/BackpackCrudModelService.php
@@ -2,53 +2,72 @@
 
 namespace Webfactor\Laravel\Generators\Services;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Webfactor\Laravel\Generators\Contracts\ServiceAbstract;
 use Webfactor\Laravel\Generators\Contracts\ServiceInterface;
 
 class BackpackCrudModelService extends ServiceAbstract implements ServiceInterface
 {
-    protected $relativeToBasePath = 'app/Models';
-
-    private $fillable;
+    protected $key = 'crudModel';
 
     public function call()
     {
-        $this->command->call('make:crud-model', [
-            'name' => $this->getName($this->command->entity),
-        ]);
-
+        $this->generateMigrationFile();
         $this->addGeneratedFileToIdeStack();
-        $this->fillFillableAttributeInGeneratedModelFromSchema();
     }
 
     /**
-     * @param string $entity
+     * Generate the crud model and save it according to specified naming.
+     *
+     * @return void
+     */
+    protected function generateMigrationFile(): void
+    {
+        try {
+            $stub = $this->filesystem->get($this->naming->getStub());
+        } catch (FileNotFoundException $exception) {
+            $this->command->error('Could not find stub file: ' . $this->naming->getStub());
+        }
+
+        $this->replaceClassName($stub);
+        $this->replaceTableName($stub);
+        $this->replaceMigrationFields($stub);
+
+        $this->filesystem->put($this->naming->getFile(), $stub);
+    }
+
+    /**
+     * Replace the class name in stub file.
      *
      * @return string
      */
-    public function getName(string $entity): string
+    protected function replaceClassName(&$stub): void
     {
-        return ucfirst($entity);
-    }
-
-    private function fillFillableAttributeInGeneratedModelFromSchema()
-    {
-        $modelFile = end($this->command->filesToBeOpened);
-
-        $model = $this->filesystem->get($modelFile);
-        $model = str_replace('__fillable__', $this->getFillableFromSchema(), $model);
-        $this->filesystem->put($modelFile, $model);
+        $stub = str_replace('__migration_class__', $this->naming->getClassName(), $stub);
     }
 
     /**
+     * Replace the table name in stub file.
+     *
      * @return string
      */
-    private function getFillableFromSchema()
+    protected function replaceTableName(&$stub): void
     {
-        $this->command->migration->getStructure()->each(function ($field) {
-            $this->fillable .= "'" . $field->getName() . "',\n";
-        });
+        $stub = str_replace('__table_name__', $this->naming->getTableName(), $stub);
+    }
 
-        return $this->fillable;
+    /**
+     * Generate migration fields in stub file.
+     *
+     * @return string
+     */
+    protected function replaceMigrationFields(&$stub): void
+    {
+        $stub = str_replace('__migration_fields__', $this->generateMigrationFields(), $stub);
+    }
+
+    protected function generateMigrationFields(): string
+    {
+        return '';
     }
 }

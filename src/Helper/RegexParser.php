bdd424ea99428486fa4f36550c14d19aef102001
Thomas Swonke <thomas.swonke@webfactor.de>
2018-06-22 21:20:43 +0200

    fix parameter order of str_contains()

--- src/Helper/RegexParser.php
+++ src/Helper/RegexParser.php
@@ -6,13 +6,13 @@ class RegexParser
 {
     public static function parseParenthesis(string $string): array
     {
-        if (str_contains('(', $string) && str_contains(')', $string)) {
+        if (str_contains($string, '(') && str_contains($string, ')')) {
             preg_match('/(.*)(\((.*)\))/', $string, $match);
         }
 
         return [
             'left' => $match[1] ?? $string,
-            'inside' => $match[2] ?? '',
+            'inside' => $match[3] ?? '',
         ];
     }
 }

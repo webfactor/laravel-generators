76326e2a968ae57f7650abb5e83b39cf7499c5b4
Thomas Swonke <thomas.swonke@webfactor.de>
2018-06-26 00:47:30 +0200

    add DocBlock to ShortSyntaxArray

--- src/Helper/ShortSyntaxArray.php
+++ src/Helper/ShortSyntaxArray.php
@@ -4,6 +4,14 @@ namespace Webfactor\Laravel\Generators\Helper;
 
 class ShortSyntaxArray
 {
+    /**
+     * Parses an array and returns a string representation of this array with short syntax []
+     *
+     * @param array $expression
+     * @param bool $removeNumericIndex
+     * @param int $indent
+     * @return string
+     */
     public static function parse(array $expression, $removeNumericIndex = true, $indent = 4): string
     {
         $object = json_decode(str_replace(['(', ')'], ['&#40', '&#41'], json_encode($expression)), true);
